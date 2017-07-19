fb44a9cb0de1d83c936c9d157cd00f0592271915
geekzl <1103307205@qq.com>
2017-07-19 15:33:47 +0800

    article detail api

--- app/js/comp/article.js
+++ app/js/comp/article.js
@@ -8,14 +8,14 @@ define('comp/article', function(require, exports, module) {
     var Vue = require('vue');
     var $ = require('jquery');
     var tpl = require('template/article');
+    var router = require('mods/router');
 
     var data = {
-        items: [{
-            uid: "1",
-            title: "白领的幻觉",
-            time: "2017年6月4日 22:22",
-            cont: "现在有一个很大的误区就是：办公室的白领们自以为自己的表现优于自己的父母，其实这不过是因为经济结构转型造成的误会而已。现在在公司的格子间里面哼哧哼哧做PPT的那些人，和当年踩着缝纫机的女工们，其实没有本质区别。"
-        }]
+        title: '这里一片荒芜',
+        username: '没作者',
+        cont: '没内容',
+        time: '时间没了',
+        intro: '介绍丢了'
     };
 
     var comp = Vue.component('blog-article', {
@@ -25,39 +25,48 @@ define('comp/article', function(require, exports, module) {
         },
         methods: {
             // 请求文章数据接口
-            reqArticleDataApi: function(e) {
-                // var _this = this;
+            reqArticleDataApi: function(id) {
+                var _this = this;
                 $.ajax({
-                    // url: '',
-                    // type: 'GET',
-                    // dataType: 'jsonp',
-                    // data: {
-                    //     page: e,
-                    //     pagesize: 10
-                    // },
-                    // success: function(res) {
-                    //     // console.log(res.bean.data);
-                    //     var flag = res.flag;
-                    //     if (flag == "200") {
-                    //         _this.list = res.bean.data;
-                    //         _this.pagingData.total = res.bean.total;
-                    //         _this.pagingData.page = res.bean.page;
-                    //         // 初始化页码
-                    //         _this.init();
-                    //     } else {
-                    //         _this.list = [];
-                    //         _this.pagingData.total = 0;
-                    //         _this.pagingData.page = 0;
-                    //         _this.init();
-                    //     }
-                    // }
+                    url: 'http://blog.feroad.com/article/getArticleDetails/' + id,
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function(res) {
+                        var flag = res.result.status;
+                        if (flag) {
+                            _this.title = res.result.data.title;
+                            _this.username = res.result.data.userName;
+                            _this.time = _this.transferTime(res.result.data.createTime);
+                            _this.cont = res.result.data.content;
+                            _this.intro = res.result.data.introduction;
+                        } else {
+                            console.log('接口请求返回错误');
+                        }
+                    },
+                    error: function() {
+                        // 请求地址不存在，则跳转到文章列表
+                        router.replace({
+                            path: '/article'
+                        });
+                    }
                 });
-            }
+            },
+            // 时间戳转换
+            transferTime: function(unixTime) {
+                var date = new Date(unixTime * 1000);
+                var Y = date.getFullYear() + '-';
+                var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+                var D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate()) + ' ';
+                var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+                var m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+                var s = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds());
+                return Y + M + D + h + m + s;
+            },
         },
         created: function() {
             console.log('article加载');
-            // 后期接口放这里，请求一遍接口就完成初始化了
-            // this.reqArticleDataApi();
+            var article_id = router.currentRoute.params.id;
+            this.reqArticleDataApi(article_id);
         }
     });
     return comp;
