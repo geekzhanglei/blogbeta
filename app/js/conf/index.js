5534688e0c8f2ca1753bcc5c94494afb4ec07ad4
geekzl <1103307205@qq.com>
2017-12-05 15:31:51 +0800

    add console tips

--- app/js/conf/index.js
+++ app/js/conf/index.js
@@ -26,6 +26,7 @@ define('conf/index', function(require, exports, module) {
         el: '#blog',
         router: router,
         created: function() {
+            console.log("这渣代码是我写的\r\n 你居然还要扒开了看\r\n我表示略尴尬\r\n 非要看源码的话：\r\n\r\n\r\n%cfollow me %c https://github.com/geekzhanglei/blogbeta", "color:red", "color:green");
             document.addEventListener('visibilitychange', function() {
                 if (document.visibilityState == "hidden") {
                     document.title = "这里有bug，快看！";
